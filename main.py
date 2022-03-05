29632700ffb72d15196ba9ac5e328f790b385186
Arad Zulti <aradzulti at gmail>
2022-03-05 19:33:00 +0200

    Make keyboard intrrupt include imports

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-from powerline_shell import powerline_shell
-
-
-if __name__ == "__main__":
-    try:
+try:
+    from powerline_shell import powerline_shell
+    if __name__ == "__main__":
         powerline_shell.powerline_shell()
-    except KeyboardInterrupt:
-        pass
+except KeyboardInterrupt:
+    pass
